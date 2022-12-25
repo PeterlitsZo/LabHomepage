ae4c69018ba56271943ecd70db54f47be73eb05b
frelion <frelionsun@gmail.com>
2022-12-25 11:25:08 +0800

    middleware Error()

--- backend/handler/middleware/PermissionsMiddleware.go
+++ backend/handler/middleware/PermissionsMiddleware.go
@@ -15,19 +15,19 @@ func NewPermissionsMiddleware(CheckFunc func(user *databaseModel.User) bool) gin
 		if token, ok := g.Request.Header["Authorization"]; !ok {
 			g.AbortWithError(http.StatusUnauthorized, handlerError.AuthorizationIsEmpty)
 			g.JSON(http.StatusUnauthorized, gin.H{
-				"message": handlerError.AuthorizationIsEmpty,
+				"message": handlerError.AuthorizationIsEmpty.Error(),
 			})
 			return
 		} else if len(token) == 0 {
 			g.AbortWithError(http.StatusUnauthorized, handlerError.AuthorizationIsEmpty)
 			g.JSON(http.StatusUnauthorized, gin.H{
-				"message": handlerError.AuthorizationIsEmpty,
+				"message": handlerError.AuthorizationIsEmpty.Error(),
 			})
 			return
 		} else if jwt := util.GetBearerToken(token[0]); jwt == "" {
 			g.AbortWithError(http.StatusUnauthorized, handlerError.TokenIsInvalid)
 			g.JSON(http.StatusUnauthorized, gin.H{
-				"message": handlerError.AuthorizationIsEmpty,
+				"message": handlerError.AuthorizationIsEmpty.Error(),
 			})
 			return
 		} else if user, err := util.ParseJWT(jwt); err != nil {
@@ -45,13 +45,13 @@ func NewPermissionsMiddleware(CheckFunc func(user *databaseModel.User) bool) gin
 		} else if usr == nil {
 			g.AbortWithError(http.StatusUnauthorized, handlerError.UserNotExist)
 			g.JSON(http.StatusUnauthorized, gin.H{
-				"message": handlerError.UserNotExist},
-			)
+				"message": handlerError.UserNotExist.Error(),
+			})
 			return
 		} else if !CheckFunc(usr) {
 			g.AbortWithError(http.StatusUnauthorized, handlerError.PermissionDenied)
 			g.JSON(http.StatusUnauthorized, gin.H{
-				"message": handlerError.PermissionDenied,
+				"message": handlerError.PermissionDenied.Error(),
 			})
 			return
 		} else {
